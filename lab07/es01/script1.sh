499af68796c81a03ca8ce13fabefc7d61125afa8
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-12-01 16:53:43 +0100

    check params

--- lab07/es01/script1.sh
+++ lab07/es01/script1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-files=$(find $1 -type f -exec grep -H -n $2 \{} \; | tr : " " | sort -k1,1 -k2,2n)
-echo "$files" > $3
+if  [ $# -ne 3 ]
+then
+    echo "$0 directory function output_file"
+    exit 1
+fi
+
+files=$(find "$1" -type f -exec grep -H -n "$2" \{} \; | tr : " " | sort -k1,1 -k2,2n)
+echo "$files" > "$3"
